91cc1749ff00965b719411c4d3c3fc437fb1c468
Alden Lipiarski <ajlip77@gmail.com>
2022-12-01 19:52:04 -0500

    Now writes to SD card

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,17 @@
 
 #define _g_ (9.80665)
 #define FILE_BASE_NAME "FlightLog_"
+bool sdexists = false;
+const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
+char* fileName = FILE_BASE_NAME "0000.csv";
+int count = 0;
+  
+FsFile f;
+SdFs sd;
 LIS331 xl;
 
 void setup() {
-  
+  //Begin set up of LIS331
   pinMode(9,INPUT);       // Interrupt pin input
   Wire.begin();
   xl.setI2CAddr(0x19);    // This MUST be called BEFORE .begin() so 
@@ -47,28 +54,73 @@ void setup() {
                           //  which interrupt source we're configuring,
                           //  and whether to enable (true) or disable
                           //  (false) the interrupt.
+  //End set up of LIS331
+
   Serial.begin(115200);
+  
+  //Setting up SD card
+  
+  if (!sd.begin(SdioConfig(FIFO_SDIO))) { 
+    Serial.println("SD Begin Failed");
+  } else {
+    Serial.println("\nFIFO SDIO mode.");
+    while (sd.exists(fileName)) {
+        if (fileName[BASE_NAME_SIZE + 3] != '9') {
+              fileName[BASE_NAME_SIZE + 3]++;
+        } else if (fileName[BASE_NAME_SIZE + 2] != '9') {
+              fileName[BASE_NAME_SIZE + 3] = '0';
+              fileName[BASE_NAME_SIZE + 2]++;
+        } else if (fileName[BASE_NAME_SIZE + 1] != '9') {
+              fileName[BASE_NAME_SIZE + 2] = '0';
+              fileName[BASE_NAME_SIZE + 3] = '0';
+              fileName[BASE_NAME_SIZE + 1]++;
+        } else if (fileName[BASE_NAME_SIZE] != '9') {
+              fileName[BASE_NAME_SIZE + 1] = '0';
+              fileName[BASE_NAME_SIZE + 2] = '0';
+              fileName[BASE_NAME_SIZE + 3] = '0';
+              fileName[BASE_NAME_SIZE]++;
+        } else {
+              Serial.println("Can't create file name");
+        }
+    }
+    f = sd.open(fileName, FILE_WRITE);
+    Serial.print("Writing to: ");
+    Serial.println(fileName);
+    if (!f) {
+        Serial.println("Failed opening file.");
+    }
+    sdexists = true;
+  } //End setting up SD card
 }
 
 void loop() {
   
   int16_t x; int16_t y; int16_t z;
   xl.readAxes(x, y, z);
-  x = xl.convertToG(200,x) * _g_; //Acceleration in the x in terms of m/s^2
-  y = xl.convertToG(200,y) * _g_; //Acceleration in the y in terms of m/s^2
-  z = xl.convertToG(200,z) * _g_; //Acceleration in the z in terms of m/s^2
+  x = xl.convertToG(400,x) * _g_; //Acceleration in the x in terms of m/s^2
+  y = xl.convertToG(400,y) * _g_; //Acceleration in the y in terms of m/s^2
+  z = xl.convertToG(400,z) * _g_; //Acceleration in the z in terms of m/s^2
 
   if (digitalRead(9) == HIGH)
   {
     Serial.println("Interrupt");
   }
+  // Printing to SD Card
+  if (sdexists && f) {
+    f.print(x); f.print(","); f.print(y); f.print(","); f.print(z);
+  } else {
+    Serial.println("Either no file or the SD card is not inserted correctly");
+  }
 
-  bool sdexists = false;
-  const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
-  char* fileName = FILE_BASE_NAME "0000.csv";
-  
-  FsFile f;
-  SdFs sd;
+  if (count % 15 == 0) {
+
+    if (sdexists && f) {
+      f.close();
+      f = sd.open(fileName, FILE_WRITE);
+    }
+  }
+
+  count += 1;
 
   
 }
